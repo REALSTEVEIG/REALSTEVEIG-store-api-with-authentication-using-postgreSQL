4bb8d1b7f5f2af108f3878e8f823e9c239c5bd47
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-12-03 22:09:43 +0100

    display updates

--- controllers/products.js
+++ controllers/products.js
@@ -39,7 +39,9 @@ exports.getSingleProduct = async (req, res) => {
 
 exports.updateProduct = async (req, res) => {
     try {
-        const product = await Products.update({name : req.body.name, price : req.body.price, image : req.body.image}, {where : {id : req.params.id}})
+        const product = await Products.update(
+            {name : req.body.name, price : req.body.price, image : req.body.image}, 
+            {where : {id : req.params.id}, returning : true, plain : true})
         res.status(StatusCodes.OK).json({product})
     } catch (error) {
         console.log(error)
